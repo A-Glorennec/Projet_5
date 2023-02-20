404037c54b2059c693f04ec2e88faaad7640253a
Anaïs.Glorennec <zans_69@hotmail.fr>
2023-02-20 16:42:28 +0100

    .

--- front/js/cart.js
+++ front/js/cart.js
@@ -25,7 +25,7 @@ function getCartFromLocalStorage() {
         console.error(error);
     } finally {
         // Si les données récupérées sont nulles, on initialise localCart à un tableau vide 
-        if (localCart === null){
+        if (localCart === null) {
             localCart = [];
         }
         else {
@@ -50,7 +50,7 @@ async function getProductCatalog() {
         return data;
     }
     catch {
-        console.error(error);
+        console.error(err);
     }
 }
 
@@ -63,8 +63,7 @@ async function showCart(cart, catalog) {
         for (let i = 0; i < cart.length; i++) {
             let item = cart[i];
 
-            productData = catalog.find(product => product._id === item.id);
-
+            productData = catalog.find(product => product._id === item._id);
             createCartItem(item, productData);
         }
 
@@ -80,14 +79,14 @@ function setTotalQuantity(cart, catalog) {
     // Récupération de tous les éléments HTML
     let totalQuantity = 0;
     let totalPrice = 0;
-    console.log(catalog);
+    //console.log(catalog);
     // Boucle pour calculer le prix des produits du panier par rapport à la quantité selectionnée
     for (let i = 0; i < cart.length; i++) {
         let item = cart[i];
 
         totalQuantity += parseInt(item.quantity);
 
-        productData = catalog.find(product => product._id === item.id);
+        productData = catalog.find(product => product._id === item._id);
         totalPrice += parseInt(item.quantity * productData.price);
     }
 
@@ -111,18 +110,18 @@ function setListenerToQuantityInput(cart, catalog) {
 
             } else {
                 // récupérer les informations d'id et de couleur depuis le HTML
-                let idItem = this.closest(".cart__item").dataset._id;
-                let colorItem = this.closest(".cart__item").dataset.colors;
+                let idItem = this.closest(".cart__item").dataset.id;
+                let colorItem = this.closest(".cart__item").dataset.color;
                 // Chercher si le produit est déjà dans le panier en comparant son id et sa couleur à ceux déjà dans le panier 
-                let itemInCart = cart.find((e) => e.colors == colorItem && e._id === idItem);
+                let itemInCart = cart.find((item) => item.color == colorItem && item._id === idItem);
                 console.log(itemInCart);
 
                 // Si le panier est vide, on le créer
                 if (itemInCart === undefined) {
-                    cart.push({ _id: idItem, colors: colorItem, quantity: parseInt(this.value)});
+                    cart.push({ _id: idItem, color: colorItem, quantity: parseInt(this.value) });
                     // Si il y a déjà un produit identique dans le panier, on met à jour sa quantité
                 } else {
-                    cart.find(item => item._id === idItem).quantity = parseInt(this.value);
+                    itemInCart.quantity = parseInt(this.value);
                 }
 
                 // mettre à jour le localStorage
@@ -148,7 +147,7 @@ function setListenerToRemoveButton(cart, catalog) {
             let idItem = cartItem.dataset._id;
             let colorItem = cartItem.dataset.colors;
             // Cherche l'objet à supprimer en comparant son id et sa couleur avec les produits dans le panier et le supprime
-            let itemToDelete = cart.find((e) => e.colors == colorItem && e._id === idItem);
+            let itemToDelete = cart.find((item) => item.colors == colorItem && item._id === idItem);
             let itemIndexToDelete = cart.indexOf(itemToDelete);
             console.log(itemIndexToDelete);
             cart.splice(itemIndexToDelete, 1);
@@ -179,7 +178,7 @@ function displayEmptyCart() {
 function createCartItem(item, productData) {
     let article = document.createElement('article');
     article.className = "cart__item";
-    article.setAttribute("data-id", item.id);
+    article.setAttribute("data-id", item._id);
     article.setAttribute("data-color", item.color);
     document.querySelector("#cart__items").appendChild(article);
     article.innerHTML += `<div class="cart__item__img">
@@ -202,4 +201,166 @@ function createCartItem(item, productData) {
                                 </div>
                               </div>`;
 };
+
+/////////////////////////////Formulaire//////////////////////////////////////////
+
+// Récupération des éléments HTML pour les inputs
+const firstNameInput = document.getElementById("firstName");
+const lastNameInput = document.getElementById("lastName");
+const adressInput = document.getElementById("address");
+const cityInput = document.getElementById("city");
+const emailInput = document.getElementById("email");
+const orderButton = document.getElementById("order");
+
+// Récupération des éléments HTML pour les messages d'erreur
+const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+const addressErrorMsg = document.getElementById("addressErrorMsg");
+const cityErrorMsg = document.getElementById("cityErrorMsg");
+const emailErrorMsg = document.getElementById("emailErrorMsg");
+
+// Regex pour valider le formualaire
+const regexFirstName = /^[a-zA-ZéèêëôöîïçÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßþ]+$/i;
+const regexLastName = regexFirstName;
+const regexAddress = /^[a-zA-Z0-9\s,.'-]{3,}$/;
+const regexCity = regexFirstName;
+const regexEmail = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+
+// Création d'un évènement sur le bouton commander qui se déclenche au clic
+orderButton.addEventListener("click", function (event) {
+    // Empêche le formulaire d'être envoyer si il est vide
+    event.preventDefault();
+
+    // Récupération de la valeur des inputs du formulaire
+    let firstName = firstNameInput.value;
+    let lastName = lastNameInput.value;
+    let address = adressInput.value;
+    let city = cityInput.value;
+    let email = emailInput.value;
+
+    // Fonction pour vérifier les données saisies par l'utilisateur et les valider
+    function validateOrder() {
+
+        let cart = getCartFromLocalStorage();
+        
+        // Message d'erreur si la saisie est incorrecte
+        if (!firstName || !regexFirstName.test(firstName)) {
+            firstNameErrorMsg.innerHTML = "Veuillez saisir un prénom valide";
+            return false;
+
+        } else if (!lastName || !regexLastName.test(lastName)) {
+            lastNameErrorMsg.innerHTML = "Veuillez saisir un nom valide";
+            return false;
+
+        } else if (!address || !regexAddress.test(address)) {
+            addressErrorMsg.innerHTML = "Veuillez saisir une adresse valide";
+            return false;
+
+        } else if (!city || !regexCity.test(city)) {
+            cityErrorMsg.innerHTML = "Veuillez saisir une ville correcte";
+            return false;
+
+        } else if (!email || !regexEmail.test(email)) {
+            emailErrorMsg.innerHTML = " Veuillez saisir une adresse e-mail valide";
+            return false;
+        }
+        // Si toutes les saisies sont correctes, on créer l'objet contact
+        else {
+            let contact = {
+                firstName: firstName,
+                lastName: lastName,
+                address: address,
+                city: city,
+                email: email,
+            };
+            // Tableau vide pour stocker les article à commander
+            let products = [];
+            // Pour chaque éléments du panier on push l'id dans le tableau products
+            for (let item of cart) {
+                products.push(item._id);
+                console.log(item);
+            }
+            console.log(products);
+            // Contient les informationsde contact
+            let finalOrderItem = { contact, products };
+           
+            // Requête à l'api pour envoyer la commande
+            // Attention aux produits vides
+
+            const orderId = fetch("http://localhost:3000/api/products/order", {
+                method: "POST",
+                
+                body: JSON.stringify(finalOrderItem),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+                
+            });
+            // On redirige vers la page de confirmation avec le numéro d'id de la commande 
+            orderId.then(async function (response) {
+                // réponse de l'API //
+                
+                const itemId = await response.json();
+                
+                //renvoi vers la page de confirmation avec l'ID de commande //
+                window.location.href = `confirmation.html?orderId=${itemId.orderId}`;
+                
+            }) .catch(error  => {
+                console.error(error);
+            } )
+        }
+    }
+    validateOrder();
+});
+
+
+
+
+
+/*
+async function submitOrder(contact) {
+    let products = getSelectedProductId(cartToOrder);
+    let finalOrderObject = { contact, products };
+    let orderId = await sendOrderRequest(finalOrderObject);
+    redirectToConfirmationPage(orderId);
+}
+
+
+// Fonction pour récupérer les ids des produits
+function getSelectedProductId (){
+    // Récupération du panier depuis le localStorage
+    cart = getCartFromLocalStorage();
+    // Création du tableau de produits vide 
+    let products = [];
+
+    for (item of cart) {
+        products.push(item._id)        
+    }
+    return products;    
+}
+
+// Fonction pour envoyer la commande
+async function sendOrderRequest(finalOrderObject) {
+    const orderIdResponse = await fetch("http://localhost:3000/api/products/order", {
+        method : "POST",
+        body : JSON.stringify(finalOrderObject),
+        headers : {
+            "Content-type" : "application/json",
+        },
+    });
+
+    if (orderIdResponse.ok) {
+        const orderIdData = await orderIdResponse.json();
+        return orderIdData.orderId;
+
+    } else {
+        console.error(err);
+    }
+}
+
+// Fonction pour rediriger vers la confirmation de commande
+function redirectToConfirmationPage(orderId) {
+    window.location.href = `confirmation.html?orderId=${orderId}`;
+}*/
+
 
