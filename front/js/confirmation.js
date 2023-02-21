5416fca2f9c4bbcd63250826f2a9cb21989aa113
Anaïs.Glorennec <zans_69@hotmail.fr>
2023-02-21 19:45:35 +0100

    .

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,6 +2,7 @@
 let url = new URL(location.href); 
 let itemId = url.searchParams.get("orderId"); 
 
+// Affichage du numéro de commande
 const orderId = document.getElementById("orderId");
 orderId.innerHTML = `${itemId}`; 
 console.log(itemId);
@@ -9,15 +10,3 @@ console.log(itemId);
 ////// On vide le LocalStorage /////////
 localStorage.clear();
 
-/*let url = window.location.href;
-let newUrl = new URL(url);
-let searchParams = newUrl.searchParams;
-let id = searchParams.get("orderId");
-
-console.log(id); */
-
-/*let url = new URL(window.location.href);
-  let itemId = url.searchParams.get("orderId");
-  if (itemId != undefined) {
-    document.querySelector("#orderId").textContent = itemId;
-  };*/
